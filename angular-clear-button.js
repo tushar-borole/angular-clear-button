28054269a4e441144bf62826eca7cdc41b0edab1
fortuneteller <andrey.v.konst@gmail.com>
2015-11-20 17:01:23 +0300

    add ie9 comapatibility

--- angular-clear-button.js
+++ angular-clear-button.js
@@ -5,37 +5,31 @@
 angular.module('angular-clear-button', []).directive('clearBtn', ['$parse', function ($parse) {
     return {
         link: function (scope, elm, attr, ngModelCtrl) {
-            console.log(elm.height())
-            var top = elm.height() / 2
+            var top = elm.height() / 2;
             elm.wrap("<div style='position: relative'></div>");
             var btn = '<span id=' + Math.round(Math.random() * 1000000000) + ' class="searchclear ng-hide glyphicon glyphicon-remove-circle"></span>';
             var angularBtn = angular.element(btn);
-            angularBtn.css('top', top)
-            elm.after(angularBtn)
-                //clear the input
+            angularBtn.css('top', top);
+            elm.after(angularBtn);
+            //clear the input
             angularBtn.on("click", function () {
                 elm.val('').trigger("change");
                 $parse(attr.ngModel).assign(scope, null);
-                scope.$apply()
-            })
-
+                scope.$apply();
+            });
 
             // show  clear btn  on focus
-
             elm.bind('focus keyup change paste propertychange', function (blurEvent) {
-
                 if (elm.val() && elm.val().length > 0) {
-                    angularBtn.removeClass("ng-hide")
+                    angularBtn.removeClass("ng-hide");
                 } else {
-                    angularBtn.addClass("ng-hide")
+                    angularBtn.addClass("ng-hide");
                 }
-
             });
-             // remove  clear btn  on focus
+            // remove  clear btn  on focus
             elm.bind('blur', function (blurEvent) {
-
                 if (!angularBtn.is(":hover"))
-                    angularBtn.addClass("ng-hide")
+                    angularBtn.addClass("ng-hide");
             });
         }
     };
